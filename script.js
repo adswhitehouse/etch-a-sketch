b69eb0dd898f3adca0528363ae3d01e6674c8189
Adam Whitehouse <adswhitehouse95@gmail.com>
2025-01-30 20:49:05 +0000

    Added functionality for the divs to randomly change color once the mouse enters it.

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 const container = document.querySelector(".container");
-  
-for(let i = 1; i < 257; i++) {
+
+for (let i = 1; i < 257; i++) {
   const div = document.createElement("div");
   div.classList.add("square");
-  container.appendChild(div)
-  }
+  container.appendChild(div);
+}
 
+function randomInteger(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+let r = randomInteger(255);
+let g = randomInteger(255);
+let b = randomInteger(255);
+
+let divs = container.children;
+let divsArray = Array.from(divs);
+
+for (let i = 0; i < divsArray.length; i++) {
+  divsArray[i].addEventListener("mouseover", function () {
+    divsArray[i].style.backgroundColor = `rgb(${randomInteger(
+      255
+    )}, ${randomInteger(255)}, ${randomInteger(255)})`;
+  });
+}
