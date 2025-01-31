185c89288e0d1e8ec3d7848949b3502768f366f2
Adam Whitehouse <adswhitehouse95@gmail.com>
2025-01-31 14:10:40 +0000

    Grid can now be changed to the user input and the color hovering works on any size grid.

--- script.js
+++ script.js
@@ -28,8 +28,9 @@ const button = document.querySelector("button");
 
 button.addEventListener("click", () => {
   let choice = parseInt(
-    prompt("Please input a new grid size between 4 and 100."),
-    "0"
+    prompt(
+      "Please input a new grid size between 2 and 100. (If you enter an invalid number, the grid defaults to 16)"
+    )
   );
   let newDivWidth = containerWidth / choice;
   let newDivHeight = containerHeight / choice;
@@ -45,6 +46,21 @@ button.addEventListener("click", () => {
       container.appendChild(newDiv);
     }
   } else {
-    return;
+    for (let i = 1; i < 257; i++) {
+      const div = document.createElement("div");
+      div.classList.add("square");
+      container.appendChild(div);
+    }
+  }
+
+  let divs = container.children;
+  let divsArray = Array.from(divs);
+
+  for (let i = 0; i < divsArray.length; i++) {
+    divsArray[i].addEventListener("mouseover", function () {
+      divsArray[i].style.backgroundColor = `rgb(${randomInteger(
+        255
+      )}, ${randomInteger(255)}, ${randomInteger(255)})`;
+    });
   }
 });
